8c299c7b2c07e5c4020e69d4f5a846d21b89a20e
bguta <bereket17@hotmail.com>
2018-03-09 20:55:18 -0800

    added dij2

--- Dijkstra.java
+++ Dijkstra.java
@@ -47,4 +47,60 @@ public static Map<Vertex, Integer> Dij(Graph graph, Vertex start) {
 		//stuff.add(path);
 		
 	}
+
+
+	public static Deque<Vertex> Dij2(Graph graph, Vertex start, Vertex end) {
+
+		Set<Vertex> Q = new HashSet<>();
+		Map<Vertex, Integer> dis = new HashMap<>();
+		Map<Vertex, Deque<Vertex>> nodes = new HashMap<>();
+
+		for (Vertex v : graph.getVertices()) {
+			dis.put(v, Integer.MAX_VALUE - 1);
+			nodes.put(v, new ArrayDeque<>());
+			Q.add(v);
+		}
+		
+		dis.put(start, 0);
+
+		while (!Q.isEmpty()) {
+			
+			Vertex u = new Vertex("");
+			Integer min = Integer.MAX_VALUE;
+			for (Vertex v : Q) {
+				if (dis.get(v) < min) {
+					u = v;
+					min = dis.get(v);
+				}
+			}
+			//found the end
+			if( u.getLabel().equals(end.getLabel())) {
+				Deque<Vertex> p = new ArrayDeque<>();
+				
+				while(nodes.get(u).peekLast() != null) {
+					p.push(u);
+					u = nodes.get(u).pop();
+				}
+				p.push(u);
+				
+				return p;
+			}
+			Q.remove(u);
+			if(u.getLabel() != "") {
+			for( Vertex v : graph.getDownstreamNeighbors(u)) {
+				if(Q.contains(v)) {
+					int alt = dis.get(u) + 1;
+					if(alt < dis.get(v)) { //shorter path
+						dis.put(v, alt);
+						nodes.get(v).add(u);
+					}
+				}
+			}
+			}
+			
+		}
+		
+		return new ArrayDeque<Vertex>();
+		
+	}
 }
